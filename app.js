1465ec4111b08d2ec52e6c678a9fac7cf6650ed9
AlanGCdB <dev.alancarlos@gmail.com>
2023-08-31 14:56:50 -0300

    Route

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const vm = new Vue({
     data: {
         produtos: [],
         produto: false,
-        carrinho: []
+        carrinho: [],
+        mensagemAlerta: "",
+        alertaAtivo: false
     },
     filters: {
         numeroPreco(valor) {
@@ -52,6 +54,7 @@ const vm = new Vue({
             this.produto.estoque--;
             const { id, nome, preco } = this.produto;
             this.carrinho.push({ id, nome, preco });
+            this.alerta(`${nome} adicionado ao carrinho.`);
         },
         removerItem(index) {
             this.carrinho.splice(index, 1);
@@ -59,15 +62,32 @@ const vm = new Vue({
         checarLocalStorage() {
             if (window.localStorage.carrinho) { this.carrinho = JSON.parse(window.localStorage.carrinho) }
 
+        },
+        alerta(mensagem) {
+            this.mensagemAlerta = mensagem;
+            this.alertaAtivo = true;
+            setTimeout(() => {
+                this.alertaAtivo = false;
+            }, 1500);
+        },
+        router() {
+            const hash = document.location.hash;
+            if (hash) { this.fetchProduto(hash.replace("#", "")) }
         }
     },
     watch: {
+        produto() {
+            document.title = this.produto.nome || "Techno";
+            const hash = this.produto.id || "";
+            history.pushState(null, null, `#${hash}`);
+        },
         carrinho() {
             window.localStorage.carrinho = JSON.stringify(this.carrinho);
         }
     },
     created() {
         this.fetchProdutos();
+        this.router();
         this.checarLocalStorage();
     }
 
